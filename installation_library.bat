e779cd57a41e3cc9501e125dee01f826a7509602
AKAAKUNLEE <644469786@qq.com>
2025-01-21 14:27:30 +0800

    新建安装库脚本

---
+++ installation_library.bat
@@ -0,0 +1,43 @@
+@echo off
+
+REM 检查 Python-VENV 虚拟环境是否存在
+if not exist "Python-VENV\Scripts\activate" (
+    echo 无法找到 Python-VENV 虚拟环境。
+    pause
+    exit /b
+)
+
+REM 激活 Python-VENV 虚拟环境
+call Python-VENV\Scripts\activate
+if %errorlevel% neq 0 (
+    echo 激活虚拟环境失败。
+    pause
+    exit /b
+) else (
+    echo 虚拟环境 Python-VENV 已激活。
+)
+
+REM 检测并更新pip版本
+pip install --upgrade pip --dry-run >nul 2>&1
+if %errorlevel% neq 0 (
+    echo pip已经是最新版本。
+) else (
+    echo 更新pip到最新版本。
+    python.exe -m pip install --upgrade pip
+)
+
+REM 安装pyinstaller（如果尚未安装）
+pip install pyinstaller
+
+REM 检查PyInstaller是否存在
+where pyinstaller >nul 2>&1
+if %errorlevel% neq 0 (
+    echo PyInstaller 未安装。请运行：pip install pyinstaller
+    pause
+    exit /b
+)else(
+    echo PyInstaller 已安装。
+)
+
+REM 保持窗口打开并允许继续输入命令
+cmd /k
